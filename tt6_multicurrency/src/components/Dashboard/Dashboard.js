2f5783c58d7df00084a9887d2db2efe8e8ff60d8
GanNicholas <e0407417@u.nus.edu>
2022-07-22 16:31:47 +0800

    Added delete modal for wallet

--- tt6_multicurrency/src/components/Dashboard/Dashboard.js
+++ tt6_multicurrency/src/components/Dashboard/Dashboard.js
@@ -2,30 +2,27 @@ import React, { useState, useEffect, useContext } from "react";
 import Box from "@mui/material/Box";
 import { toast } from "react-toastify";
 import { AuthContext } from "../context/auth-context";
-// import {
-//   Paper,
-//   Table,
-//   TableBody,
-//   TableCell,
-//   TableContainer,
-//   TableHead,
-//   TableRow,
-// } from "@mui/material";
+import { Table, Modal } from "rsuite";
 
-import { Table } from "rsuite";
+import axios from "axios";
 
 import { Form, Row, Col, Card, Button } from "react-bootstrap";
+import { PencilSquare, TrashFill, FiletypeJpg } from "react-bootstrap-icons";
 
 const Dashboard = () => {
-  const [wallet, setWallet] = useState();
+  const [listOfWallet, setListOfWallet] = useState();
   const [exchangeRate, setExchangeRate] = useState();
   const { HeaderCell, Cell, Column } = Table;
   const auth = useContext(AuthContext);
+
+  const [openDeleteConfirmModal, setOpenDeleteConfirmModal] = useState(false);
+  const [walletToDelete, setWalletToDelete] = useState();
+
   var userId = 0;
-  if(auth.userId !== 1){
+  if (auth.userId !== 1) {
     userId = JSON.parse(sessionStorage.getItem("session")).userId;
-  } else{
-    userId = auth.userId
+  } else {
+    userId = auth.userId;
   }
 
   const exchangeRateLocal = [
@@ -138,6 +135,44 @@ const Dashboard = () => {
     };
   }, []);
 
+  const deleteWallet = (event) => {
+    event.preventDefault();
+    const walletId = walletToDelete.id;
+    const response = await axios
+    .delete(
+      `${process.env.REACT_APP_API_FLASK}/delete/wallet/${walletId}`
+    )
+    .catch(function(error) {
+      if (
+        error !== undefined &&
+        error.response !== undefined &&
+        error.response !== null
+      ) {
+        toast.error(error.response.data);
+      }
+    });
+
+  if (
+    response !== null &&
+    response !== undefined &&
+    response.status === 200
+  ) {
+    toast.success("Deleted wallet.");
+    const oldListOfWallet = listOfWallet;
+    oldListOfWallet.filter((wallet) => {
+        if(wallet.id !== walletId){
+            return wallet;
+        }
+    });
+
+    setListOfWallet(oldListOfWallet);
+    setOpenDeleteConfirmModal(false);
+  }
+    
+
+
+  };
+
   return (
     <React.Fragment>
       <div className="mx-2 mt-4 mb-2 ">
@@ -158,13 +193,12 @@ const Dashboard = () => {
                 wordWrap
                 data={walletLocal.filter((wallet) => {
                   console.log(auth.userId);
-                //   console.log(wallet);
+                  //   console.log(wallet);
                   console.log(wallet.user_id);
                   if (wallet.user_id === userId) {
                     return wallet;
                   }
                 })}
-              
               >
                 <Column flexGrow={1}>
                   <HeaderCell>S/N</HeaderCell>
@@ -181,23 +215,34 @@ const Dashboard = () => {
                   <Cell dataKey="name" />
                 </Column>
 
-                {/* <Column width={120} fixed="right">
-            <HeaderCell>Action</HeaderCell>
+                <Column width={120} fixed="right">
+                  <HeaderCell>Action</HeaderCell>
 
-            <Cell>
-              {(rowData) => {
-                function handleAction() {
-                  alert(`id:${rowData.id}`);
-                }
-                return (
-                  <span>
-                    <a onClick={handleAction}> Edit </a> |{" "}
-                    <a onClick={handleAction}> Remove </a>
-                  </span>
-                );
-              }}
-            </Cell>
-          </Column> */}
+                  <Cell>
+                    {(rowData) => {
+                      function deleteAction() {
+                        setWalletToDelete(rowData);
+                        setOpenDeleteConfirmModal(true);
+                      }
+                      return (
+                        <span>
+                          {/* <a onClick={handleAction}> Edit </a> |{" "}
+                          <a onClick={handleAction}> Remove </a> */}
+                          <TrashFill
+                            style={{
+                              fontSize: "140%",
+                              cursor: "pointer",
+                              marginLeft: "10px",
+                              marginRight: "10px",
+                            }}
+                            onClick={deleteAction}
+                            color="red"
+                          />
+                        </span>
+                      );
+                    }}
+                  </Cell>
+                </Column>
               </Table>
             </Card.Body>
           </Card>
@@ -240,28 +285,66 @@ const Dashboard = () => {
                   <HeaderCell>Rate</HeaderCell>
                   <Cell dataKey="rate" />
                 </Column>
-                {/* <Column width={120} fixed="right">
-            <HeaderCell>Action</HeaderCell>
-
-            <Cell>
-              {(rowData) => {
-                function handleAction() {
-                  alert(`id:${rowData.id}`);
-                }
-                return (
-                  <span>
-                    <a onClick={handleAction}> Edit </a> |{" "}
-                    <a onClick={handleAction}> Remove </a>
-                  </span>
-                );
-              }}
-            </Cell>
-          </Column> */}
               </Table>
             </Card.Body>
           </Card>
         </div>
       </div>
+      <Modal
+        id="deleteConfirmationModal"
+        backdrop={"static"}
+        keyboard={false}
+        open={openDeleteConfirmModal}
+        onClose={() => setOpenDeleteConfirmModal(false)}
+        size={"md"}
+        overflow={false}
+        // style={{
+        //   width: width,
+        //   height: height,
+        //   margin: margin,
+        // }}
+      >
+        <Modal.Header>
+          <Modal.Title style={{ fontWeight: "bold" }}>
+            Confirm delete your wallet
+          </Modal.Title>
+          <Modal.Body
+            style={{ width: "95%", marginLeft: "auto", marginRight: "auto" }}
+          >
+            <p>
+              Are you sure you wish to delete your wallet{" "}
+              <strong>
+                {walletToDelete !== undefined && walletToDelete !== null
+                  ? walletToDelete.name
+                  : ""}
+              </strong>
+              ?
+            </p>
+          </Modal.Body>
+          <Modal.Footer>
+            <div className="align-right">
+              <Button
+                type={"button"}
+                onClick={deleteWallet}
+                variant="danger"
+                style={{
+                  textTransform: "uppercase",
+                  fontWeight: "bold",
+                  marginRight: "10px",
+                }}
+              >
+                Delete
+              </Button>
+              <Button
+                onClick={() => setOpenDeleteConfirmModal(false)}
+                appearance="subtle"
+              >
+                Cancel
+              </Button>
+            </div>
+          </Modal.Footer>
+        </Modal.Header>
+      </Modal>
     </React.Fragment>
   );
 };
